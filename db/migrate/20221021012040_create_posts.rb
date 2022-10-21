935912e2b021826bf3d3b7563c309d5d57e17b93
RobynSpaulding <robynm27@gmail.com>
2022-10-20 19:09:08 -0700

    adds Posts controller and Post model

---
+++ db/migrate/20221021012040_create_posts.rb
@@ -0,0 +1,12 @@
+class CreatePosts < ActiveRecord::Migration[7.0]
+  def change
+    create_table :posts do |t|
+      t.string :title
+      t.date :date
+      t.text :body
+      t.string :image
+
+      t.timestamps
+    end
+  end
+end
